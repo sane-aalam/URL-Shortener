84764e85d6d909c78f3efd043e8f100a2b78b772
Sane <saneraza78692@gmail.com>
2025-07-07 15:55:32 +0530

    chore: setup Express server and connect to MongoDB

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,26 @@
 import express from "express";
-const app = express();
-const port = 3000;
 import { nanoid } from "nanoid";
+import dotenv from "dotenv";
+const app = express();
+const PORT = process.env.PORT;
+import connectDB from "./config/mongodb.config.js";
+dotenv.config();
 
 app.get("/createURL", (req, res) => {
   const generateString = nanoid(7);
   res.json({
     msg: "successfully called done",
     str: generateString,
+    return: 1,
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT || 3000, () => {
+      console.log(`⚙️ Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
