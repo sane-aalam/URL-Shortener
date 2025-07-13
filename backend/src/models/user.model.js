6a8d8d1ebdee4d6d24b5460ed84d0481148e87c9
Sane <saneraza78692@gmail.com>
2025-07-13 15:36:44 +0530

    feat: add user model schema with validator

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,51 @@
+// import mongoose
+// create Schema & create model
+// export model
+
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validator(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("emailId is not Valid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("emailId is not Valid");
+        }
+      },
+    },
+    avtar: {
+      type: String, // dummy avtar pic
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUZr8J_NnQJMD6bl8-AdMIwE0eP_3jOmCv6xL59PRTuwllTH4uiiU-9h0YdR31H2c09jc&usqp=CAU",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
